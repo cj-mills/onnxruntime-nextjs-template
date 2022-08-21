21f4735ab48d5aeab8ce7991aa7eb5ae9c79b766
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-08-21 13:07:24 -0700

    Update modelHelper.ts

--- utils/modelHelper.ts
+++ utils/modelHelper.ts
@@ -3,15 +3,15 @@ import _ from 'lodash';
 import { imagenetClasses } from '../data/imagenet';
 
 export async function runSqueezenetModel(preprocessedData: any): Promise<[any, number]> {
-  
+
   // Create session and set options. See the docs here for more options: 
   //https://onnxruntime.ai/docs/api/js/interfaces/InferenceSession.SessionOptions.html#graphOptimizationLevel
   const session = await ort.InferenceSession
-                          .create('./_next/static/chunks/pages/squeezenet1_1.onnx', 
-                          { executionProviders: ['webgl'], graphOptimizationLevel: 'all' });
+    .create('./_next/static/chunks/pages/squeezenet1_1.onnx',
+      { executionProviders: ['wasm'], graphOptimizationLevel: 'all' });
   console.log('Inference session created')
   // Run inference and get results.
-  var [results, inferenceTime] =  await runInference(session, preprocessedData);
+  var [results, inferenceTime] = await runInference(session, preprocessedData);
   return [results, inferenceTime];
 }
 
@@ -21,18 +21,18 @@ async function runInference(session: ort.InferenceSession, preprocessedData: any
   // create feeds with the input name from model export and the preprocessed data.
   const feeds: Record<string, ort.Tensor> = {};
   feeds[session.inputNames[0]] = preprocessedData;
-  
+
   // Run the session inference.
   const outputData = await session.run(feeds);
   // Get the end time to calculate inference time.
   const end = new Date();
   // Convert to seconds.
-  const inferenceTime = (end.getTime() - start.getTime())/1000;
+  const inferenceTime = (end.getTime() - start.getTime()) / 1000;
   // Get output results with the output name from the model export.
   const output = outputData[session.outputNames[0]];
   //Get the softmax of the output data. The softmax transforms values to be between 0 and 1
   var outputSoftmax = softmax(Array.prototype.slice.call(output.data));
-  
+
   //Get the top 5 results.
   var results = imagenetClassesTopK(outputSoftmax, 5);
   console.log('results: ', results);
@@ -55,9 +55,9 @@ function softmax(resultArray: number[]): any {
  */
 export function imagenetClassesTopK(classProbabilities: any, k = 5) {
   const probs =
-      _.isTypedArray(classProbabilities) ? Array.prototype.slice.call(classProbabilities) : classProbabilities;
+    _.isTypedArray(classProbabilities) ? Array.prototype.slice.call(classProbabilities) : classProbabilities;
 
-  const sorted = _.reverse(_.sortBy(probs.map((prob: any, index: number) => [prob, index]), (probIndex: Array<number> ) => probIndex[0]));
+  const sorted = _.reverse(_.sortBy(probs.map((prob: any, index: number) => [prob, index]), (probIndex: Array<number>) => probIndex[0]));
 
   const topK = _.take(sorted, k).map((probIndex: Array<number>) => {
     const iClass = imagenetClasses[probIndex[1]];
